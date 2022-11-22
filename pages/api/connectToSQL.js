6472ab809e1d9408e6934882d96da7914946fac4
MattiBSH <cph-mh829@cphbusiness.dk>
2022-11-22 23:15:58 +0100

    I think its a good lil thing for the website

--- pages/api/connectToSQL.js
+++ pages/api/connectToSQL.js
@@ -9,7 +9,9 @@ export default async function getItemToFeature(req, res) {
         password:"mypassword",
     })
 
-    const [rows, fields] = await dbConnection.execute("SELECT * from item order by rand() limit 1");
+    //const [rows, fields] = await dbConnection.execute("SELECT * from item order by rand() limit 1 join restaurant on item.restaurant_id as restaurant.id on restaurant.id = item.restaurant_id");
+    const [rows, fields] = await dbConnection.execute("SELECT restaurant.name as restaurant_name,restaurant.phone,item.name as item_name,item.price from restaurant,item where restaurant.restaurant_id = item.restaurant_id order by rand() limit 1");
+    
     res.status(200).json(rows);
 
 
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import NavigationMenu from '../widgets/navigation-menu'
+import { useState,useEffect } from 'react'
 
 export default function Home() {
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const res = await fetch('/api/connectToSQL');
+      const data = await res.json();
+      setItem(data[0]);
+      console.log(data);
+    };
+    return () => {
+      fetchItem();
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +32,35 @@ export default function Home() {
           Welcome to 
         </h1>
         <h1 className={styles.subtitle}>Food To Go</h1>
+        
+        { item.restaurant_name != null&&
+        <div className={styles.dailyItem}> 
+        
+        <h1 className={styles.featuredItem}>Featured Item</h1>
+        
+        <table>
+            <thead>
+            <tr>
+              <th className={styles.th}>Item name</th>
+              <th className={styles.th}>Price</th>
+              <th className={styles.th}>Restaurant Name</th>
+              <th className={styles.th}>Phone</th>
+
+            </tr>
+            </thead>
+            <tr>   
+              <td className={styles.td}>{item.item_name}</td>
+              <td className={styles.td}>{item.price}</td>
+              <td className={styles.td}>{item.restaurant_name}</td>
+              <td className={styles.td}>{item.phone}</td>
+            </tr>
+        </table>
+        </div>
+}
+        
+    
       <div className={styles.block}></div>
+        
       </main>
       
 
